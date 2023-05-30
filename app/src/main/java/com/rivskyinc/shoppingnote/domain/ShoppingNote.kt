4d6e5d2a9e84838623294a8547c7da00e1c8a64f
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-05-30 19:08:22 +0300

    added ShopRepositoryIml.kt

--- app/src/main/java/com/rivskyinc/shoppingnote/domain/ShoppingNote.kt
+++ app/src/main/java/com/rivskyinc/shoppingnote/domain/ShoppingNote.kt
@@ -1,8 +1,14 @@
 package com.rivskyinc.shoppingnote.domain
 
 data class ShoppingNote(
-    val id : Int,
     val name : String,
     val count : Int,
-    val enabled : Boolean
-)
+    val enabled : Boolean,
+    var  id : Int = UNDEFINED
+){
+
+    companion object{
+
+        const val UNDEFINED = -1
+    }
+}
