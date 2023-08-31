99438f77210e23be92cc600ffcb4e8adfb000736
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-08-31 03:36:08 +0300

    added empty DetailShopItemActivity

---
+++ app/src/main/java/com/rivskyinc/shoppingnote/presentation/DetailShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.rivskyinc.shoppingnote.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rivskyinc.shoppingnote.R
+
+class DetailShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_shop_item)
+    }
+}
