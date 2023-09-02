86fd2bdd992d4fb20d24968254b560fee65d0364
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-02 20:07:10 +0300

    added initViews in DetailShopItemActivity.kt

--- app/src/main/java/com/rivskyinc/shoppingnote/presentation/DetailShopItemActivity.kt
+++ app/src/main/java/com/rivskyinc/shoppingnote/presentation/DetailShopItemActivity.kt
@@ -4,15 +4,34 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
 import com.rivskyinc.shoppingnote.R
 
 class DetailShopItemActivity : AppCompatActivity() {
+
+    private lateinit var nameInput : TextInputEditText
+    private lateinit var countInput: TextInputEditText
+    private lateinit var editName : EditText
+    private lateinit var editCount : EditText
+    private lateinit var buttonSave : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_shop_item)
-
+        initViews()
         val getIntentString = intent.getStringExtra(EXTRA_SCREEN_MODE)
         val getIntentInt =  intent.getIntExtra(MODE_ID, 0 )
+    }
+
+    private fun initViews() {
+        nameInput = findViewById(R.id.til_name)
+        countInput = findViewById(R.id.til_count)
+        editName = findViewById(R.id.edit_name)
+        editCount = findViewById(R.id.edit_count)
+        buttonSave = findViewById(R.id.button_save)
+
     }
 
 
